923a6c81ec297a5d73a80d40696386eab4da9677
smlblbn <smlblbn@gmail.com>
2018-06-17 22:46:41 +0300

    minor bug fixed

--- pca.py
+++ pca.py
@@ -1,69 +1,71 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+
 
 def show(img):
-	imgplot = plt.imshow(img.reshape(231,195), cmap='gray')
-	plt.show()
+    plt.imshow(img.reshape(231, 195), cmap='gray')
+    plt.show()
+
 
 def showEigenface(ver, hor, X):
-	L = []
-	R = []
+    L = []
+    R = []
 
-	for i in range(ver):
-		for j in range(hor):
-			L.append(X[i*hor+j,:].reshape(231,195))
-		R.append(np.hstack(L))
-		L = []
+    for i in range(ver):
+        for j in range(hor):
+            L.append(X[i * hor + j, :].reshape(231, 195))
+        R.append(np.hstack(L))
+        L = []
 
-	A = np.vstack(R)
+    A = np.vstack(R)
 
-	imgplot = plt.imshow(A, cmap='gray')
-	plt.show()
+    plt.imshow(A, cmap='gray')
+    plt.show()
 
-def findPrincipalComponents(dataset,k):
 
-	mean = np.mean(dataset,axis=0)
-	std = np.std(dataset,axis=0)
+def findPrincipalComponents(dataset, k):
+    mean = np.mean(dataset, axis=0)
+    std = np.std(dataset, axis=0)
 
-	dataset2 = (dataset-mean)/std
+    dataset2 = (dataset - mean) / std
 
-	cov = np.dot(dataset2, dataset2.T)
-	e,v = np.linalg.eigh(cov)
+    cov = np.dot(dataset2, dataset2.T)
+    e, v = np.linalg.eigh(cov)
 
-	# sort the eigenvalue and same time eigen vector and choose them
-	idx = np.argsort(e)[::-1]
-	v = v[:,idx]
-	e = e[idx]
-	v = v[:, :k]
+    # sort the eigenvalue and same time eigen vector and choose them
+    idx = np.argsort(e)[::-1]
+    v = v[:, idx]
+    e = e[idx]
+    v = v[:, :k]
 
-	u = np.dot(dataset2.T, v)
+    u = np.dot(dataset2.T, v)
 
-	return u
+    return u
 
-def pcaCompress(image,component):
-	c = np.dot(component.T, image)
-	q = np.dot(component,c)
 
-	return q
+def pcaCompress(image, component):
+    c = np.dot(component.T, image)
+    q = np.dot(component, c)
+
+    return q
+
 
 dataset = np.load('FaceImages.npy')
-image = [dataset[0,:],dataset[1,:]]
-karray = [16,32,64]
+image = [dataset[0, :], dataset[1, :]]
+karray = [16, 32, 64]
 
 for i in range(len(image)):
-	for j in range(len(karray)):
-		pca = findPrincipalComponents(dataset,karray[j])
+    for j in range(len(karray)):
+        pca = findPrincipalComponents(dataset, karray[j])
 
-		precon = pcaCompress(image[i],pca)
+        precon = pcaCompress(image[i], pca)
 
-		show(image[i])
-		show(precon)
+        show(image[i])
+        show(precon)
 
-		if karray[j] == 16:
-			showEigenface(4,4,pca.T)
-		elif karray[j] == 32:
-			showEigenface(4,8,pca.T)
-		elif karray[j] == 64:
-			showEigenface(8,8,pca.T)
-
+        if karray[j] == 16:
+            showEigenface(4, 4, pca.T)
+        elif karray[j] == 32:
+            showEigenface(4, 8, pca.T)
+        elif karray[j] == 64:
+            showEigenface(8, 8, pca.T)
---
+++ pca_sklearn.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+
+
+def show(img):
+    plt.imshow(img.reshape(231, 195), cmap='gray')
+    plt.show()
+
+
+def pca(x):
+    x = (x - x.mean(axis=0)) / np.std(dataset, axis=0)
+
+    num_observations, num_dimensions = x.shape
+    if num_dimensions > 100:
+        eigenvalues, eigenvectors = np.linalg.eigh(np.dot(x, x.T))
+        v = (np.dot(x.T, eigenvectors).T)[::-1]
+        s = np.sqrt(eigenvalues)[::-1]
+    else:
+        u, s, v = np.linalg.svd(x, full_matrices=False)
+
+    return v, s
+
+if __name__ == "__main__":
+    dataset = np.load('FaceImages.npy')
+    k = 16
+    mean = np.mean(dataset, axis=0)
+    std = np.std(dataset, axis=0)
+    image = dataset[0, :]
+
+    pca = PCA(n_components=k)
+    pca.fit(dataset)
+    x = pca.transform(image.reshape(1, -1))
+    y = pca.inverse_transform(x)
+    recon = np.dot(pca.components_.T, x.T)
+
+    show(y)
+    show(np.squeeze(recon) + pca.mean_)
